d565de07be72545b5631dc60660cd2343170e773
macrusso <mac.rusek1@gmail.com>
2018-11-06 15:34:55 +0000

    update/delete user + postman

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,3 +8,31 @@ export const getUsers = async (req, res, next) => {
     return next(err);
   }
 };
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = {
+      name: req.body.name,
+      profileImageUrl: req.body.profileImageUrl,
+    };
+    const foundUser = await db.User.findByIdAndUpdate(
+      req.params.post_id,
+      updatedUser,
+      { new: true }
+    );
+    return res.status(200).json(foundUser);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const deletedUser = await db.User.findByIdAndDelete(req.params.post_id);
+    return res.status(200).json({
+      id: req.params.post_id,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
