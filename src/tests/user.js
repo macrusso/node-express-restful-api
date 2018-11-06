aba7c9e2ea225f823a1050f61c8f6b1fdb97ef3d
macrusso <mac.rusek1@gmail.com>
2018-11-06 15:58:47 +0000

    user tests

--- src/tests/user.js
+++ src/tests/user.js
@@ -1,39 +1,113 @@
-import mongoose from "mongoose";
-import chaiHttp from "chai-http";
-import chai from "chai";
-import app from "../index";
-import { token } from "./auth";
+import mongoose from 'mongoose';
+import chaiHttp from 'chai-http';
+import chai from 'chai';
+import app from '../index';
+import { token, testUser } from './auth';
 
 chai.should();
 chai.use(chaiHttp);
 
-describe("User", () => {
-  describe("/GET all users", () => {
-    it("it should GET all users", done => {
+describe('User', () => {
+  describe('/GET all users', () => {
+    it('it should GET all users', done => {
       chai
         .request(app)
-        .get("/api/users")
-        .set("Authorization", `Bearer ${token}`)
+        .get('/api/users')
+        .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.a("array");
+          res.body.should.be.a('array');
           done();
         });
     });
   });
 
-  describe("/GET all users - no token", () => {
-    it("it should fail to GET all users", done => {
+  describe('/GET all users - no token', () => {
+    it('it should fail to GET all users', done => {
       chai
         .request(app)
-        .get("/api/users")
+        .get('/api/users')
         .end((err, res) => {
           res.should.have.status(401);
-          res.body.should.be.a("object");
-          res.body.should.have.property("error");
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
           res.body.error.should.have
-            .property("message")
-            .eql("Please log in first");
+            .property('message')
+            .eql('Please log in first');
+          done();
+        });
+    });
+  });
+
+  describe('/PATCH update user', () => {
+    it('it should PATCH update user', done => {
+      const updatedUser = {
+        name: 'test user',
+        profileImageUrl: 'awesome new test user - updated',
+      };
+      chai
+        .request(app)
+        .patch(`/api/users/${testUser._id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(updatedUser)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name').eql(updatedUser.name);
+          done();
+        });
+    });
+  });
+
+  describe('/PATCH update user - no token', () => {
+    it('it should fail to PATCH update user', done => {
+      const updatedUser = {
+        name: 'test user',
+        profileImageUrl: 'awesome new test user - updated',
+      };
+      chai
+        .request(app)
+        .patch(`/api/users/${testUser._id}`)
+        .send(updatedUser)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          res.body.error.should.have
+            .property('message')
+            .eql('Please log in first');
+          done();
+        });
+    });
+  });
+
+  describe('/DEL delete user', () => {
+    it('it should DEL delete user', done => {
+      chai
+        .request(app)
+        .del(`/api/users/${testUser._id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('id').eql(testUser._id);
+          done();
+        });
+    });
+  });
+
+  describe('/DEL delete user - no token', () => {
+    it('it should fail to DEL delete user', done => {
+      chai
+        .request(app)
+        .del(`/api/users/${testUser._id}`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          res.body.error.should.have
+            .property('message')
+            .eql('Please log in first');
           done();
         });
     });
