f5a79b0a35d2473795fdb5b4b8efe86bd6810264
Mac Rusek <mac.rusek@pm.me>
2020-03-03 18:38:20 +0000

    update tests

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { loginRequired } from "./middleware/auth";
 
 dotenv.config();
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
